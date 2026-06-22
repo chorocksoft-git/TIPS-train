944e9f0e2a56fb03c7b7e7cf98aa2577a15d043c
wonyoung.jo <joker@chorock.co.kr>
2026-06-22 15:16:34 +0900

    드론 특화 학습 파라미터 최적화

--- train.py
+++ train.py
@@ -2,19 +2,20 @@ from ultralytics import YOLO
 
 # model = YOLO("models/yolo11n.pt") # 기존 nano 모델
 # 복잡한 각도를 학습하려면 모델 크기를 키우는 것도 고려 (s 또는 m)
-model = YOLO("models/yolo11s.pt") 
+model = YOLO("models/yolo11m.pt")
 
 results = model.train(
-    data="datasets/v10/tacticalist.yaml",
+    data="datasets/v14/tacticalist.yaml",
 
    # imgsz=[608, 1088],
    # rect=True,
     imgsz=640,
     epochs=200, # 어려운 데이터일수록 에포크를 좀 더 늘리는 게 좋습니다.
-    batch=32,
+    batch=128,
     device=0,
     amp=True,
-    cache=True,
+    cache='disk',
+    workers=4,
 
     # --- [핵심 수정: 기하학적 증강 추가] ---
     perspective=0.001,  # 원근감 (트리거캠 시점 흉내) - 값을 조금씩 변경해보세요
@@ -30,8 +31,13 @@ results = model.train(
     copy_paste=0.1,
     auto_augment="randaugment",
 
+    # [추가 증강 파라미터]
+    bgr=0.1,        # 10% 확률로 색상 채널을 뒤집음 (파란 하늘 외에 역광 등 다양한 색감 대응)
+    hsv_s=0.5,      # 채도 변환 폭 증가 (안개 낀 날씨 흉내)
+    hsv_v=0.5,      # 명도 변환 폭 증가 (눈부신 환경 흉내)
+
     # 후반 안정화
     close_mosaic=30, # 에포크가 늘었으니 이것도 약간 늘려줍니다.
     patience=60,
-    name="result_v10_s",
+    name="result_v14",
 )
